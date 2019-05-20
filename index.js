88abe8d1d096ffb919e11d85a8fd6a1b87f11f62
Hardik <hdnakarani@gmail.com>
2019-05-20 16:05:59 +0530

    create login route and  protected route

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Sequelize = require("sequelize");
+const jwt = require("jsonwebtoken");
+
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "wowwow";
+
+// lets create our strategy for web token
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log("payload received", jwt_payload);
+  let user = getUser({ id: jwt_payload.id });
+
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+// use the strategy
+passport.use(strategy);
 
 const app = express();
+// initialize passport with express
+app.use(passport.initialize());
 
-//initialize an instance of sequelize
+// parse application/json
+app.use(bodyParser.json());
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const Sequelize = require("sequelize");
+
+// initialze an instance of Sequelize
 const sequelize = new Sequelize({
   database: "users_db",
   username: "root",
@@ -13,14 +45,13 @@ const sequelize = new Sequelize({
   dialect: "mysql"
 });
 
-//check the database connections
-
+// check the databse connection
 sequelize
   .authenticate()
-  .then(() => console.log("connection establish successfully"))
-  .catch(err => console.error("Unable to connect to the database : ", err));
+  .then(() => console.log("Connection has been established successfully."))
+  .catch(err => console.error("Unable to connect to the database:", err));
 
-//create user model
+// create user model
 const User = sequelize.define("user", {
   email: {
     type: Sequelize.STRING
@@ -36,13 +67,12 @@ const User = sequelize.define("user", {
   }
 });
 
-//create table with user model
-
+// create table with user model
 User.sync()
-  .then(() => console.log("User Table created Successfully."))
-  .catch(err => console.error("Did you enter wrong database credentials?"));
+  .then(() => console.log("User table created successfully"))
+  .catch(err => console.log("oooh, did you enter wrong database credentials?"));
 
-//helper functions to work on the database
+// create some helper functions to work on the database
 const createUser = async ({ email, fullname, username, password }) => {
   return await User.create({ email, fullname, username, password });
 };
@@ -57,26 +87,17 @@ const getUser = async obj => {
   });
 };
 
-//parse application json
-app.use(bodyParser.json());
-//parse application/x-www-form-urlencoded
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//add a basic route
-app.get("/", (req, res) => {
-  res.json({
-    message: "Express is up!"
-  });
+// set some basic routes
+app.get("/", function(req, res) {
+  res.json({ message: "Express is up!" });
 });
 
-//get all users
-
+// get all users
 app.get("/users", function(req, res) {
   getAllUsers().then(user => res.json(user));
 });
 
-//register new user
+// register route
 app.post("/register", function(req, res, next) {
   const { email, fullname, username, password } = req.body;
   createUser({ email, fullname, username, password }).then(user =>
@@ -84,9 +105,36 @@ app.post("/register", function(req, res, next) {
   );
 });
 
-//start the app
-app.listen(3000, () => {
-  console.log("====================================");
+//login route
+app.post("/login", async function(req, res, next) {
+  const { email, password } = req.body;
+  if (email && password) {
+    let user = await getUser({ email });
+    if (!user) {
+      res.status(401).json({ message: "No such user found" });
+    }
+    if (user.password === password) {
+      // from now on we'll identify the user by the id and the id is the
+      // only personalized value that goes into our token
+      let payload = { id: user.id };
+      let token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.json({ msg: "ok", token: token });
+    } else {
+      res.status(401).json({ msg: "Password is incorrect" });
+    }
+  }
+});
+
+// protected route
+app.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  function(req, res) {
+    res.json("Success! You can now see this without a token.");
+  }
+);
+
+// start app
+app.listen(3000, function() {
   console.log("Express is running on port 3000");
-  console.log("====================================");
 });
