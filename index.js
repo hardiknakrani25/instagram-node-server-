43cee1143a8758182b001693e44d529a37f7f0c2
Hardik <hdnakarani@gmail.com>
2019-05-20 14:31:36 +0530

    initialie connection  mysql and sequelize orm create model user. create table of user model. add helper functions

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+
+const app = express();
+
+//initialize an instance of sequelize
+
+const sequelize = new Sequelize({
+  database: "users_db",
+  username: "root",
+  password: "",
+  dialect: "mysql"
+});
+
+//check the database connections
+
+sequelize
+  .authenticate()
+  .then(() => console.log("connection establish successfully"))
+  .catch(err => console.error("Unable to connect to the database : ", err));
+
+//create user model
+const User = sequelize.define("user", {
+  email: {
+    type: Sequelize.STRING
+  },
+  fullname: {
+    type: Sequelize.STRING
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+});
+
+//create table with user model
+
+User.sync()
+  .then(() => console.log("User Table created Successfully."))
+  .catch(err => console.error("Did you enter wrong database credentials?"));
+
+//helper functions to work on the database
+const createUser = async ({ email, fullname, username, password }) => {
+  return await User.create({ email, fullname, username, password });
+};
+
+const getAllUsers = async () => {
+  return await User.findAll();
+};
+
+//parse application json
+app.use(bodyParser.json());
+//parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//add a basic route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Express is up!"
+  });
+});
+
+//start the app
+app.listen(3000, () => {
+  console.log("====================================");
+  console.log("Express is running on port 3000");
+  console.log("====================================");
+});
